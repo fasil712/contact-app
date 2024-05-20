558fdc44d4691d31daf0011848e889dca29c4662
fasil712 <fasilgetie12@gmail.com>
2024-05-20 10:49:02 -0400

    first commit

---
+++ contactapi/src/main/java/com/digitalequb/contactapi/model/Contact.java
@@ -0,0 +1,11 @@
+package com.digitalequb.contactapi.model;
+
+
+public class Contact {
+    private int id;
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+
+}
